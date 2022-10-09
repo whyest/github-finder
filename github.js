98cb0b269055abe3d7264afb24566a6639a7ca3a
Denis <31589533+whyest@users.noreply.github.com>
2022-10-09 15:38:19 +0300

    Add files via upload
    Add missing files to the project.

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+	constructor() {
+		this.client_id = '80e220284c0c9a4079b3';
+		this.client_secret = 'e5f608a7188525c3faf0e9424b192f6aee4288f7';
+		this.repos_count = 7;
+		this.repos_sort = 'created asc';
+	}
+
+	async getUser(user) {
+		const profileResponse = await fetch(
+			`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+		);
+
+		const reposResponse = await fetch(
+			`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+		);
+
+		const profile = await profileResponse.json();
+		const repos = await reposResponse.json();
+
+		return {
+			profile,
+			repos,
+		};
+	}
+}
